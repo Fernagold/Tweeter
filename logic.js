4118607aefa9d1fed78bb550d4290521b62d0877
Fernagold <fernagold@gmail.com>
2018-11-28 17:27:32 +0200

    All logic done!

--- logic.js
+++ logic.js
@@ -19,33 +19,77 @@ const Tweeter = function () {
         ]
     }
 ]
+    let idInit = 2
+    let commentIdInit = 6
+    const idPostIndex = function(indexString){
+        for(let i in posts){
+            if(posts[i].id == indexString){
+                return i;
+            }
+        }
+        return -1;
+    }
+    const idPostCommentIndex = function(CommentString, index){
+        if(index == -1){
+            return -1;
+        }
+        for(let i in posts[index].comments){
+             if(posts[index].comments[i].id == CommentString){
+                 return i
+            }
+         }
+            return -1;
+        }
+        
     const getPost = function() {
     return console.log(posts)
 }
     const addPost = function(text) {
-        let idInit = 1
-        for(add in posts){
-           add = idInit++
-        }
-           posts.push({text: text, id: "p" + idInit, comments: []})
-           return addPost
+        idInit ++   
+        
+        posts.push({text: text, id: "p" + idInit, comments: []})
+        return addPost
        }
         
         
     
-    const removePost = function(index){
+    const removePost = function(IdIndexNumber){
+        let index = idPostIndex(IdIndexNumber);
+        if(index != -1){
         posts.splice(index, 1)
-        return removePost
-               
+        }
+        else{
+        return console.log("The id is not found")
+        }          
+    }
+    const addComment = function(text, com){
+        commentIdInit++
+        let index = idPostIndex(com)
+        if(index != -1){                
+        posts[index].comments.push({id: "c" + commentIdInit, text: text})}
+        else{
+            console.log("This Post Id doesn't exist")
+        }
+        return addComment
+    }
+    const removeComment = function(pString, cString){
+        let indexPost = idPostIndex(pString)
+        let indexComment = idPostCommentIndex(cString, indexPost)
+        if(indexPost != -1 && indexComment != -1){
+        posts[indexPost].comments.splice(indexComment, 1)
+    }   else {
+        console.log("Post comment Id not found")
+    }
+    
+        return removeComment
     }
     return {
         getPost: getPost,
         addPost: addPost,
         removePost: removePost,
+        addComment: addComment,
+        removeComment: removeComment
     }
 }
 const result = Tweeter()
-result.addPost("Tweet test")
-result.getPost()
-result.removePost(2)
-result.getPost()
+
